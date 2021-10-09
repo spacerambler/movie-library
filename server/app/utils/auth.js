c81a4b0a47c85c93745179d0d25e28a5ecc53994
Tyler Lawson <tylaw93@yahoo.com>
2021-10-09 14:05:43 -0500

    Started jwt

--- server/app/utils/auth.js
+++ server/app/utils/auth.js
@@ -1,11 +1,11 @@
-// import jwt from ('jsonwebtoken');
+import jwt from ('jsonwebtoken');
 
-// const secret = 'mysecretssshhhhhhh';
-// const expiration = '2h';
+const secret = 'login';
+const expiration = '2h';
 
-// module.exports = {
-//   signToken: function ({ email, username, _id }) {
-//     const payload = { email, username, _id };
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   },
-// };
+module.exports = {
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
