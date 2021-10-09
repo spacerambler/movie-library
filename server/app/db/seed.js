9d50b43a6f4de49f8ef156d7c778782588b66144
Tyler Lawson <tylaw93@yahoo.com>
2021-10-09 14:05:27 -0500

    Added seed data

--- server/app/db/seed.js
+++ server/app/db/seed.js
@@ -1,11 +1,41 @@
 // TODO: Import the models
 import client from "./client.js";
+import User from "../models/User.js";
+import Movie from "../models/Movie.js";
 
 client
   .connect()
-  .then(() => {
-    // TODO: Seed your database here!
-  })
+  .then(() => { 
+    Movie.insertMany([
+      {
+        _id: 123,
+        rating: 5,
+        tags: ["Horror", "Action"]
+      },
+      {
+        _id: 12,
+        rating: 5,
+        tags: ["Comedy", "Action"]
+      }
+    ])
+    .then((film) => 
+    Promise.all([
+      User.create({
+        name: "Tyler",
+       email: "tylaw93@yahoo.com",
+       password: "12345",
+       movies: [{
+         film: [film[0]._id, film[1]._id],
+       },
+     ],
+      }),
+      User.create({
+        name: "Elijah",
+        email: "eholt@testmail.com",
+        password: "password12345",
+      }),
+    ])
+  )
   .catch((err) => {
     console.error("MongoDB connection error:", err.message);
   })
@@ -13,3 +43,6 @@ client
     console.log("🗃️ 🌱");
     client.close();
   });
+
+
+  
