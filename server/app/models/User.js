6adb05048bc26cc86d39933fac31b7375db21841
Tyler Lawson <tylaw93@yahoo.com>
2021-10-12 19:36:46 -0500

    Added passward bcrypt

--- server/app/models/User.js
+++ server/app/models/User.js
@@ -1,5 +1,7 @@
 import Movie from "./Movie.js"
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import config from "../config.js"
 
 
 const userSchema = new mongoose.Schema({
@@ -18,8 +20,25 @@ const userSchema = new mongoose.Schema({
         minlength: 5,
       },
       movies: [Movie.schema]
-
 })
+userSchema.pre(
+  "save",
 
+  // ⚠️ Need to use arrow function to preserve 'this' context
+  async function (next) {
+    // ⚠️ Don't do this unnecessarily!
+    if (this.isNew || this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, config.saltRounds);
+    }
+
+    next();
+  }
+);
+
+userSchema.methods.validatePassword =
+  // ⚠️ STILL Need to use arrow function to preserve 'this' context
+  function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 export default mongoose.model("User", userSchema);
 
